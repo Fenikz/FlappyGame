d0f7874b517468eef1a589d6e9ccd96f3cfe17dd
Fenikz <dsmakarov@gmail.com>
2016-04-07 22:21:16 +0300

    Initial commit

---
+++ core/src/com/sirpomidor/sprites/Tube.java
@@ -0,0 +1,64 @@
+package com.sirpomidor.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
+/**
+ * Created by Dmitry on 07.04.2016.
+ */
+public class Tube {
+
+    public static final int FLUCTUATION = 130;
+    public static final int TUBE_GAP = 100;
+    public static final int LOWEST_OPENING = 120;
+
+    public static final int TUBE_WIDTH = 52;
+
+    private Texture topTube, botTube;
+    private Vector2 posTopTube, posBotTube;
+    private Random random;
+    private Rectangle boundsTop, boundsBot;
+
+    public Tube(float x) {
+        topTube = new Texture("toptube.png");
+        botTube = new Texture("bottomtube.png");
+        random = new Random();
+
+        posTopTube = new Vector2(x, random.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube = new Vector2(x, posTopTube.y - TUBE_GAP - botTube.getHeight());
+
+        boundsTop = new Rectangle(posTopTube.x, posTopTube.y, topTube.getWidth(), topTube.getHeight());
+        boundsBot = new Rectangle(posBotTube.x, posBotTube.y, botTube.getWidth(), botTube.getHeight());
+    }
+
+    public void reposition(float x) {
+        posTopTube.set(x, random.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube.set(x, posTopTube.y - TUBE_GAP - botTube.getHeight());
+        boundsTop.setPosition(posTopTube.x, posTopTube.y);
+        boundsBot.setPosition(posBotTube.x, posBotTube.y);
+
+    }
+
+    public boolean collides(Rectangle player) {
+        return player.overlaps(boundsTop) || player.overlaps(boundsBot);
+    }
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBotTube() {
+        return botTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBotTube() {
+        return posBotTube;
+    }
+}
